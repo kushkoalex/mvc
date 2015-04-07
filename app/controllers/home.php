8e627f48192c6adfc189650af7c6a41638beece7
Alexander Kushko <kushko.alex@gmail.com>
2015-04-07 23:27:29 +0300

    ---

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -14,8 +14,8 @@ class Home extends Controller
 
     public function index($name = '')
     {
-        //$user = $this->user;
-        //$user->name = $name;
+        $user = $this->user;
+        $user->name = $name;
 
         $this->view('home/index', ['name' => $user->name]);
 
